4b5dbf6212c56ceffed8337e17b69853e59533b6
Greg Ingram <shaggygi97@gmail.com>
2018-12-14 21:02:40 -0500

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        public static object MSBuildLocator { get; private set; }
+
+        static async Task Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis.MSBuild;
+using System.Linq;
+using System.Windows;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Test_Click(object sender, RoutedEventArgs e)
+        {
+            MSBuildLocator.RegisterDefaults();
+            var workspace = MSBuildWorkspace.Create();
+
+            // TODO: Point to location of project here.
+            string projectPath = @".......\TryingAnalyzerReferences\ConsoleApp\ConsoleApp.csproj";
+
+            var project = await workspace.OpenProjectAsync(projectPath);
+            var allLanguageAnalyzers = project.AnalyzerReferences.SelectMany(r => r.GetAnalyzersForAllLanguages());
+            var languageSpecificAnalyzers = project.AnalyzerReferences.SelectMany(r => r.GetAnalyzers(project.Language));
+        }
+    }
+}
